13897bcd9f0fcca97c723f7cfa63a43aafce397c
MEspinosaGH <mespinosa3@miuandes.cl>
2019-04-04 14:12:00 -0300

    lab finished

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,15 +2,17 @@ class Ticket < ApplicationRecord
   belongs_to :ticket_type
   belongs_to :order
 
-  before_create do
-    event_date = TicketType.where(event: Event).where(id: self.ticket_type).select("event_start_date")
+  before_save do
+    event_date = Event.where(id: TicketType.where(id: self.ticket_type).select("event_id")).select("start_date")
     sbeve = Order.where(id: self.order).select("created_at")
     if !event_date.empty?
-      if event_date < Date.today
-    	  errors.add(:start_date, "can't add ticket for past event")
+      if event_date[0].start_date < Date.today
+    	  errors.add(:start_date, "can't add ticket for past event") 
+	  throw :abort
       end
-      if sbeve < Date.today
+      if sbeve[0].created_at > event_date[0].start_date
           errors.add(:start_date, "can't buy ticket of a finished event")
+	  throw :abort
       end
     end
   end
